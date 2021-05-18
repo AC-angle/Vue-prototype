d4939abcc130875235dc535e34981b208fb8d440
你好2007 <2501482523@qq.com>
2021-05-18 16:25:50 +0800

    -

--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>页面1888888888</div>
+    <div>页面188888888800000</div>
 </template>
 <script>
 export default {
   mounted() {
     
     // todo
-    document.title='         '
+    document.title='                                                                              .'
 
   }
 };
