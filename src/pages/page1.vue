68fa5f4c8a3cbd32300ae8ac23528a6c94ae29bb
你好2007 <2501482523@qq.com>
2021-05-27 14:40:22 +0800

    -

--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>页面188888888800000</div>
+  <div>
+    页面188888888800000
+
+    <a href="sccba802://platformapi/startapp"> 打开APP </a>
+  </div>
 </template>
 <script>
 export default {
   mounted() {
-    
     // todo
-    document.title='\u200E'
-
-  }
+    document.title = "\u200E";
+  },
 };
 </script>
